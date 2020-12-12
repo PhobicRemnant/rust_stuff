facf8c9bbfc5353dec8e41f72cc565ce1ddf204b
PhobicRemnant <sergio.m.gonzalez.a.94@gmail.com>
2020-12-12 17:05:49 +0100

    Guessing game example

---
+++ tutorials/guessing_game/target/rls/debug/build/getrandom-b921cc6dbb8ffd23/build_script_build-b921cc6dbb8ffd23.d
@@ -0,0 +1,5 @@
+/home/greatceph/Documents/myRepos/rust_stuff/tutorials/guessing_game/target/rls/debug/build/getrandom-b921cc6dbb8ffd23/build_script_build-b921cc6dbb8ffd23: /home/greatceph/.cargo/registry/src/github.com-1ecc6299db9ec823/getrandom-0.1.15/build.rs
+
+/home/greatceph/Documents/myRepos/rust_stuff/tutorials/guessing_game/target/rls/debug/build/getrandom-b921cc6dbb8ffd23/build_script_build-b921cc6dbb8ffd23.d: /home/greatceph/.cargo/registry/src/github.com-1ecc6299db9ec823/getrandom-0.1.15/build.rs
+
+/home/greatceph/.cargo/registry/src/github.com-1ecc6299db9ec823/getrandom-0.1.15/build.rs:
---
+++ tutorials/guessing_game/target/rls/debug/build/libc-52659ccb4d3eddd0/build_script_build-52659ccb4d3eddd0.d
@@ -0,0 +1,5 @@
+/home/greatceph/Documents/myRepos/rust_stuff/tutorials/guessing_game/target/rls/debug/build/libc-52659ccb4d3eddd0/build_script_build-52659ccb4d3eddd0: /home/greatceph/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.81/build.rs
+
+/home/greatceph/Documents/myRepos/rust_stuff/tutorials/guessing_game/target/rls/debug/build/libc-52659ccb4d3eddd0/build_script_build-52659ccb4d3eddd0.d: /home/greatceph/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.81/build.rs
+
+/home/greatceph/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.81/build.rs:
