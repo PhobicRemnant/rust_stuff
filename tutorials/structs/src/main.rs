c1568882c72a63e97f53e55da2bc62f41f1e1c00
PhobicRemnant <sergio.m.gonzalez.a.94@gmail.com>
2021-03-17 10:29:17 +0100

    Final struct method added

--- tutorials/structs/src/main.rs
+++ tutorials/structs/src/main.rs
@@ -98,6 +98,14 @@ impl Rectangle{
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle{
+        Rectangle{
+            width: size,
+            height: size,
+        }
+    }
+
 }
 
 fn main() {
@@ -163,5 +171,8 @@ fn main() {
     println!("Rect1 holds Rect2: {}", rect1.can_hold(&rect2));
     println!("Rect1 holds Rect3: {}", rect1.can_hold(&rect3));
 
+    let rect4 = Rectangle::square(4);
 
+    println!("Rect4:");
+    println!(" -area: {}", rect4.area());
 }
