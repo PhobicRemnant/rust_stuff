999266a291e3188c9ab5e400984e5e7ed0cd33a8
PhobicRemnant <sergio.m.gonzalez.a.94@gmail.com>
2021-04-18 16:12:49 +0200

    Chapter 8: Collections exercises complete.

--- tutorials/workforce_database/src/main.rs
+++ tutorials/workforce_database/src/main.rs
@@ -32,20 +32,29 @@ fn main(){
         };
 
         match option {
-            1 => enter_employee(&workforce),      // Add employee to database
-            2 => display_workforce(),   // Show the worforce in an ordely manner
+            1 => enter_employee(&mut workforce),      // Add employee to database
+            2 => display_workforce(&workforce),   // Show the worforce in an ordely manner
             3 => break,                 // Break loop and exit program
             _ => println!(""),
         }
 
-        
+        // Show to user
+        println!("Press any key to continue.");
+        //Press a key to continue   
+         reader
+         .read_line(&mut input)
+         .expect("Failed to read");
+
+        }
+
+        println!("");
+        println!("Thank you for using the workforce database");
+        println!("");
     }
 
-    println!("");
-    println!("Thank you for using the workforce database");
-    println!("");
+  
     
-}
+
 
 fn display_options(){
     println!("Welcome to the workforce database");
@@ -56,7 +65,7 @@ fn display_options(){
     println!("");
 }
 
-fn enter_employee(database: &HashMap<String,Vec<String>>){
+fn enter_employee(database: &mut HashMap<String,Vec<String>>) {
     
     // Display example to user
     println!("");
@@ -72,13 +81,42 @@ fn enter_employee(database: &HashMap<String,Vec<String>>){
         .read_line(&mut line)
         .expect("Invalid input, please look at example.");
     
+    // Read line and add to vector 
     let new_entry: Vec<&str> = line.split_whitespace().collect();
-    // Order input as a 4  
+    
+    for s in &new_entry {
+        print!(" {} ",s);
+    }
+
+
+    // Order input taking into account the given example "add" and "to"  
+    if new_entry[0] == "Add" && new_entry[2] == "to" {  
+        
+        // Create a new entry if it isn't in the database already
+        database.entry(new_entry[3].to_string())
+                .or_insert(Vec::new());
+
+        // Add employee's name to the database
+        database.get_mut(new_entry[3]).unwrap().push(new_entry[1].to_string());
+        // Order employee's vector alphabetically
+        database.get_mut(new_entry[3]).unwrap().sort();
+
+        println!("{} added to {}",new_entry[1], new_entry[3]);
+    }else{ // Print to console the error
+        println!("Wrong command to insert new employee.");
+    }
 
 }
 
-fn display_workforce(){
+fn display_workforce(database: &HashMap<String,Vec<String>>){
     println!("");
     println!("Display stuff");
     println!("");
+
+    for (k,v) in database{
+        println!("{}:",k);
+        for element in v{
+            println!("      {}",element);
+        }
+    }
 }
